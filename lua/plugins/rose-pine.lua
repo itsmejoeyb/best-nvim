172080f1fb720f59251c2682fa1a550f3d1591f5
Joey <31454088+itsmejoeyb@users.noreply.github.com>
2024-11-02 01:23:33 -0400

    mmm yummy transparency

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -2,7 +2,12 @@ return {
 	"rose-pine/neovim",
 	priority = 1000,
 	name = "rose-pine",
-	config = function()
+	opts = {
+		styles = {
+			transparency = true,
+		},
+	},
+	init = function()
 		vim.cmd("colorscheme rose-pine")
 	end,
 }
